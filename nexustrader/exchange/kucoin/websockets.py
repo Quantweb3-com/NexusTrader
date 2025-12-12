8558750c9a2305857662cdf9601e6621449106a8
Jiaxiang Li <974687126@microsoft.com>
2025-12-12 17:26:35 +0800

    change: add KucoinWSClient

--- nexustrader/exchange/kucoin/websockets.py
+++ nexustrader/exchange/kucoin/websockets.py
@@ -1,46 +1,391 @@
-# https://www.kucoin.com/docs-new/websocket-api/base-info/introduction-uta
+from typing import Any, Callable, List, Literal
 
-# PING
-# {
-#   "id": "1545910590801",
-#   "type": "ping"
-# }
+from nexustrader.base import WSClient
+from nexustrader.core.entity import TaskManager
+from nexustrader.core.nautilius_core import LiveClock
+from nexustrader.exchange.kucoin.constants import KucoinAccountType
 
-# You can use timestamp_ms as the id for PING messages
-# define the user_api_pong_callback function to handle PONG responses
 
-# PONG
-# {
-#   "id": "1545910590801",
-#   "type": "pong",
-#   "timestamp": 1764215232226553
-# }
+class KucoinWSClient(WSClient):
+    def __init__(
+        self,
+        account_type: KucoinAccountType,
+        handler: Callable[..., Any],
+        task_manager: TaskManager,
+        clock: LiveClock,
+        custom_url: str | None = None,
+    ) -> None:
+        self._account_type = account_type
 
-# class KuCoinWebsocketPongMsg(Struct):
-#     type: str | None = None
+        url = custom_url or account_type.stream_url
+        if not url:
+            raise ValueError(f"WebSocket URL not supported for {account_type}")
 
-#     @property
-#     def is_pong(self) -> bool:
-#         return self.type == "pong"
+        super().__init__(
+            url=url,
+            handler=handler,
+            task_manager=task_manager,
+            clock=clock,
+            enable_auto_ping=False,
+        )
 
-# You need to define two classes
-# KucoinWSClient(WSClient)
+    async def _subscribe(self, topics: List[dict[str, str]]) -> None:
+        if not topics:
+            return
 
-# Public Channels
-# ------------------------------------------
-# Ticker SPOT | Futures https://www.kucoin.com/docs-new/3470222w0
-# def subscribe_ticker(self, symbols: List[str], trade_type: Literal["SPOT", "FUTURES"] = "SPOT") -> None:
+        new_topics: List[dict[str, str]] = []
+        for t in topics:
+            key = f"{t['topic']}|{t.get('symbol', '')}"
+            if key in self._subscriptions:
+                continue
+            self._subscriptions.append(key)
+            new_topics.append(t)
 
-# Trade
-# OrderBook
-# Kline
+        if not new_topics:
+            return
 
-# Private Channels
-# ------------------------------------------
-# Order 
-# Position
-# Balance
+        await self.connect()
 
-# KucoinWSApiClient(WSClient)
-# Add Order
-# Cancel Order
+        payload = {
+            "id": str(self._clock.timestamp_ms()),
+            "type": "subscribe",
+            "topic": new_topics[0]["topic"] + ":"
+            + ",".join({tp["symbol"] for tp in new_topics}),
+            "response": True,
+        }
+        self._send(payload)
+
+    async def _unsubscribe(self, topics: List[dict[str, str]]) -> None:
+        if not topics:
+            return
+
+        remove_topics: List[dict[str, str]] = []
+        for t in topics:
+            key = f"{t['topic']}|{t.get('symbol', '')}"
+            if key not in self._subscriptions:
+                continue
+            self._subscriptions.remove(key)
+            remove_topics.append(t)
+
+        if not remove_topics:
+            return
+
+        await self.connect()
+
+        payload = {
+            "id": str(self._clock.timestamp_ms()),
+            "type": "unsubscribe",
+            "topic": remove_topics[0]["topic"] + ":"
+            + ",".join({tp["symbol"] for tp in remove_topics}),
+            "response": True,
+        }
+        self._send(payload)
+
+    async def _manage_subscription(
+        self,
+        action: str,
+        symbols: List[str],
+        *,
+        topic: str,
+        symbol_builder: Callable[[str], str] | None = None,
+    ) -> None:
+
+        if not symbols:
+            return
+
+        symbols = [s.upper() for s in symbols]
+
+        if symbol_builder is not None:
+            built_symbols = [symbol_builder(s) for s in symbols]
+        else:
+            built_symbols = symbols
+
+        topics = [{"symbol": s, "topic": topic} for s in built_symbols]
+
+        if action == "subscribe":
+            await self._subscribe(topics)
+        else:
+            await self._unsubscribe(topics)
+
+    async def _manage_private_subscription(self, action: str, topic: str) -> None:
+
+        await self.connect()
+
+        payload = {
+            "id": str(self._clock.timestamp_ms()),
+            "type": action,
+            "topic": topic,
+            "privateChannel": True,
+            "response": True,
+        }
+        self._send(payload)
+
+    async def subscribe_trade(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/market/match",
+        )
+
+    async def unsubscribe_trade(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/market/match",
+        )
+
+    async def subscribe_kline(
+        self,
+        symbols: List[str],
+        interval: str,
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/market/candles",
+            symbol_builder=lambda s: f"{s}_{interval}",
+        )
+
+    async def unsubscribe_kline(
+        self,
+        symbols: List[str],
+        interval: str,
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/market/candles",
+            symbol_builder=lambda s: f"{s}_{interval}",
+        )
+
+    async def subscribe_futures_kline(
+        self,
+        symbols: List[str],
+        interval: str,
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/contractMarket/limitCandle",
+            symbol_builder=lambda s: f"{s}_{interval}",
+        )
+
+    async def unsubscribe_futures_kline(
+        self,
+        symbols: List[str],
+        interval: str,
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/contractMarket/limitCandle",
+            symbol_builder=lambda s: f"{s}_{interval}",
+        )
+
+    async def subscribe_futures_trade(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/contractMarket/execution",
+        )
+
+    async def unsubscribe_futures_trade(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/contractMarket/execution",
+        )
+
+    async def subscribe_book_l1(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/spotMarket/level1",
+        )
+
+    async def unsubscribe_book_l1(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/spotMarket/level1",
+        )
+
+    async def subscribe_book_l5(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/spotMarket/level2Depth5",
+        )
+
+    async def unsubscribe_book_l5(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/spotMarket/level2Depth5",
+        )
+
+    async def subscribe_futures_book_l5(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/contractMarket/level2Depth5",
+            require_futures=True,
+        )
+
+    async def unsubscribe_futures_book_l5(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/contractMarket/level2Depth5",
+            require_futures=True,
+        )
+
+    async def subscribe_book_l50(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/spotMarket/level2Depth50",
+        )
+
+    async def unsubscribe_book_l50(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/spotMarket/level2Depth50",
+        )
+
+    async def subscribe_futures_book_l50(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/contractMarket/level2Depth50",
+        )
+
+    async def unsubscribe_futures_book_l50(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/contractMarket/level2Depth50",
+        )
+
+    async def subscribe_book_incremental(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/market/level2",
+        )
+
+    async def unsubscribe_book_incremental(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/market/level2",
+        )
+
+    async def subscribe_futures_book_incremental(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "subscribe",
+            symbols,
+            topic="/contractMarket/level2",
+        )
+
+    async def unsubscribe_futures_book_incremental(
+        self,
+        symbols: List[str],
+    ) -> None:
+        await self._manage_subscription(
+            "unsubscribe",
+            symbols,
+            topic="/contractMarket/level2",
+        )
+
+
+    async def subscribe_balance(self) -> None:
+        await self._manage_private_subscription("subscribe", "/account/balance")
+
+    async def unsubscribe_balance(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/account/balance")
+
+    async def subscribe_futures_balance(self) -> None:
+        await self._manage_private_subscription("subscribe", "/contractAccount/wallet")
+
+    async def unsubscribe_futures_balance(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/contractAccount/wallet")
+
+    async def subscribe_order_v2(self) -> None:
+        await self._manage_private_subscription("subscribe", "/spotMarket/tradeOrdersV2")
+
+    async def unsubscribe_order_v2(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/spotMarket/tradeOrdersV2")
+
+    async def subscribe_order_v1(self) -> None:
+        await self._manage_private_subscription("subscribe", "/spotMarket/tradeOrders")
+
+    async def unsubscribe_order_v1(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/spotMarket/tradeOrders")
+        
+    async def subscribe_futures_positions(self) -> None:
+        await self._manage_private_subscription("subscribe", "/contract/positionAll")
+
+    async def unsubscribe_futures_positions(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/contract/positionAll")
+
+    async def subscribe_futures_orders(self) -> None:
+        await self._manage_private_subscription("subscribe", "/contractMarket/tradeOrders")
+
+    async def unsubscribe_futures_orders(self) -> None:
+        await self._manage_private_subscription("unsubscribe", "/contractMarket/tradeOrders")
+
+class KucoinWSApiClient(WSClient):
+    pass
